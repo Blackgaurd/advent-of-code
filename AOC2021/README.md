e508e9fde439f4e126e67b6fc83bad3e1958c7d6
bryan <bryan.deng002@gmail.com>
2021-12-11 23:31:14 -0500

    day 11

--- AOC2021/README.md
+++ AOC2021/README.md
@@ -3,3 +3,5 @@
 Repository of my solutions.
 
 *python is a pleasure to write in but sometimes it really screws me over* :triumph:
+
+day 11 was a pain
---
+++ AOC2021/day11: dumbo octopus/part1.py
@@ -0,0 +1,58 @@
+from functools import total_ordering
+import sys
+import os
+from pprint import pprint
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+
+sys.stdin = open(f"{DIR}/input.txt", "r")
+sys.stdout = open(f"{DIR}/part1.txt", "w")
+
+arr = [list(map(int, list(line))) for line in sys.stdin.read().splitlines()]
+n, m = len(arr), len(arr[0])
+dx = [1, 0, -1, 0, 1, 1, -1, -1]
+dy = [0, 1, 0, -1, 1, -1, 1, -1]
+
+total_flashes = 0
+
+def print_arr():
+    pprint(arr)
+    print()
+
+def count():
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] > 9:
+                return True
+    return False
+
+def flash():
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] > 9:
+                arr[i][j] = -1
+                for k in range(8):
+                    x, y = i + dx[k], j + dy[k]
+                    if 0 <= x < n and 0 <= y < m and arr[x][y] != -1:
+                        arr[x][y] += 1
+
+STEPS = 100
+for _ in range(STEPS):
+    # increment everything by 1
+    for i in range(n):
+        for j in range(m):
+            arr[i][j] += 1
+
+    # flash ones with 9 or greater
+    while count():
+        flash()
+        flash()
+
+    # set all flashed ones to 0
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] == -1:
+                total_flashes += 1
+                arr[i][j] = 0
+
+print(total_flashes)
